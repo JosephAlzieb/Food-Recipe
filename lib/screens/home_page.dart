9e47024f2d4174187681bb4a95642d5431f75278
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-01-31 23:17:10 +0100

    so now you can get the data an save it in a list to represent it

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:food_recipe/models/recipe.dart';
+import 'package:food_recipe/models/recipe_api.dart';
 import 'package:food_recipe/widgets/recipe_card.dart';
 
 class HomePage extends StatefulWidget {
@@ -7,6 +9,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  List<Recipe> _recipes;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getRecipes();
+  }
+
+  Future<void> getRecipes() async {
+    _recipes = await RecipeApi.getRecipe();
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
